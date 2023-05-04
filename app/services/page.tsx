147a19bb8e58a5f12354f60c9aade76314b24c49
Jeni <ziahrahl@gmail.com>
2023-05-04 15:30:03 -0400

    layout for cards and services page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,22 +1,44 @@
 import servicesData from 'public/data.json'
 import Card from '../../components/card/Card'
+import heroImage from 'public/images/masonryBanner.jpeg'
+import Image from 'next/image'
+import styles from './Services.module.scss'
+
+type Service = {
+  id: number
+  name: string
+  details: string[]
+  imageSrc: string
+  // Add any other properties of your service object here
+}
+
+export type CardProps = {
+  service: Service
+}
 
 export default function Services(): JSX.Element {
   const { services } = servicesData
- 
+
   return (
-    <div>
-      <div>PAGE HERO</div>
-      <h2>SERVICES</h2>
-      <ul>
-        {services.map((service, index) => (
-          <li key={index}>{service.name}</li>
-        ))}
-      </ul>
-      <div>
-        {services.map((service, index) => (
-          <Card service={service} key={index} />
-        ))}
+    <div className={styles.pageWrapper}>
+      <div className={styles.heroWrapper}>
+        <Image alt="masonry" src={heroImage} placeholder="blur" sizes="100vw" fill />
+        <div className={styles.pageTitle}>
+          <h1>SERVICES</h1>
+        </div>
+      </div>
+      <div className={styles.pageContentWrapper}>
+        <h2>Residential & Commercial</h2>
+        <ul className={styles.topSectionList}>
+          {services.map((service, index) => (
+            <li key={index}>{service.name}</li>
+          ))}
+        </ul>
+        <div className={styles.cardGrid}>
+          {services.map((service) => (
+            <Card service={service} key={service.id} />
+          ))}
+        </div>
       </div>
     </div>
   )
--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -1,22 +1,28 @@
 import React from 'react'
 import styles from './Card.module.scss'
 import Image from 'next/image'
+import { CardProps } from 'app/services/page'
 
-type Service = {
-  name: string
-  imageSrc: string
-  // Add any other properties of your service object here
-}
 
-type CardProps = {
-  service: Service
-}
+
 
 const Card = ({ service }: CardProps) => {
   return (
-    <div>
-      <Image src={service.imageSrc} alt="placeholder text" width={400} height={300}/>
+    <div className={styles.card}>
       <h2>{service.name}</h2>
+      <div className={styles.cardImageWrapper}>
+        <Image
+          src={service.imageSrc}
+          alt="placeholder text"
+          width={400}
+          height={300}
+        />
+      </div>
+      <ul>
+        {service.details.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   )
 }
