1db999e303cc3759d512ddd610228253d1faf56a
Jeni <ziahrahl@gmail.com>
2023-05-04 07:53:31 -0400

    import images for services page

---
+++ app/services/services.ts
@@ -0,0 +1,16 @@
+// export type MenuGroup = {
+//     [key: string]: MenuItem[]
+//   }
+//   export type MenuItem = {
+//     id: string
+//     name: string
+//     description: string
+//     price: string
+//     image?: StaticImageData
+//     group?: string
+//   }
+
+
+export const listOfServices = {
+    
+}
---
+++ components/card/Card.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Card.module.scss'
+
+const Card = ( ) => {
+  return (
+    <div>
+       Card Elements
+    </div>
+  )
+}
+
+export default Card
