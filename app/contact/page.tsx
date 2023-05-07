4bfb7630aa07b091fe0c165d6bd004ec2ccac338
Jeni <ziahrahl@gmail.com>
2023-05-07 18:12:18 -0400

    edit on contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,7 +4,7 @@ import styles from './Contact.module.scss'
 export default function Contact() {
   return (
     <div className={styles.contactPageWrapper}>
-      <h1>{"Let's Connect!"}</h1>
+      <h1>{`Let's Connect!`}</h1>
       <p>Discuss your project with a team member by filling out our form.</p>
       <ContactForm />
 
